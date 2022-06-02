cb87fd0c73e34f4a246a0ffbfe931a6c420b531a
Katelind Phillips <scarlettkate94@gmail.com>
2022-06-02 14:35:38 -0400

    switch statements

---
+++ switchStatements/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace switchStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite school subject?");
+            string favSubject = Console.ReadLine();
+
+            switch (favSubject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Nice choice! Math can be tough.");
+                    break;
+                case "science":
+                    Console.WriteLine("Science can be tricky.");
+                    break;
+                case "english":
+                    Console.WriteLine("Nice! English can be difficult.");
+                    break;
+                case "history":
+                    Console.WriteLine("History teachers make the best storytellers.");
+                    break;
+                case "gym":
+                    Console.WriteLine("Gym can be a fun way to get out energy.");
+                    break;
+                default:
+                    Console.WriteLine("I didn't think of that one. Kudos!");
+                    break;
+            }
+        }
+    }
+}
+
